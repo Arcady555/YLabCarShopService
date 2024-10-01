7b67e48494271fd73624df153cae7f67aca2768d
arcady555 <parfenov7333@gmail.com>
2024-10-01 06:35:13 +0300

    Test service

---
+++ homework_5/src/test/java/ru/homework_5/service/impl/LogServiceSpringImplTest.java
@@ -0,0 +1,43 @@
+package ru.homework_5.service.impl;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.homework_5.model.LineInLog;
+import ru.homework_5.repository.LogRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class LogServiceSpringImplTest {
+    LogRepository repo = mock(LogRepository.class);
+    LogServiceSpringImpl logService = new LogServiceSpringImpl(repo);
+
+    @Test
+    @DisplayName("Проверка метода findByParameters")
+    void findByParameters() {
+        LineInLog log = new LineInLog(
+                1,
+                LocalDateTime.of(2024, 1,1, 1, 1),
+                "1",
+                "action"
+        );
+        when(repo.findByParameters(
+                "1",
+                "action",
+                LocalDateTime.of(2024, 1,1, 1, 1),
+                LocalDateTime.of(2024, 2,1, 1, 1)
+        )).thenReturn(List.of(log));
+
+        List<LineInLog> result = logService.findByParameters(
+                "1", "action", "2024-01-01T01:01", "2024-02-01T01:01"
+        );
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(List.of(log), result);
+    }
+}
---
+++ homework_5/src/test/java/ru/homework_5/service/impl/OrderServiceSpringImplTest.java
@@ -0,0 +1,112 @@
+package ru.homework_5.service.impl;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import ru.homework_5.dto.OrderDTO;
+import ru.homework_5.dto.OrderDTOMapper;
+import ru.homework_5.enums.OrderStatus;
+import ru.homework_5.enums.OrderType;
+import ru.homework_5.enums.Role;
+import ru.homework_5.model.Order;
+import ru.homework_5.model.Person;
+import ru.homework_5.repository.OrderRepository;
+import ru.homework_5.service.CarService;
+import ru.homework_5.service.PersonService;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class OrderServiceSpringImplTest {
+    @Autowired
+    OrderDTOMapper dtoMapper;
+    OrderRepository repo = mock(OrderRepository.class);
+    PersonService personService = mock(PersonService.class);
+    CarService carService = mock(CarService.class);
+    OrderServiceSpringImpl orderService = new OrderServiceSpringImpl(repo, personService, carService);
+
+    @Test
+    @WithMockUser
+    @DisplayName("Метод create костыль")
+    void create() {
+        Order order = new Order(1, 1, 1, OrderType.BUY, OrderStatus.OPEN);
+
+        OrderDTO orderDTO = dtoMapper.toOrderDTO(order);
+        when(repo.save(any(Order.class))).thenReturn(order);
+        when(carService.isOwnCar(1, 1)).thenReturn(false);
+
+        Optional<Order> result = orderService.create(orderDTO);
+
+        Assertions.assertTrue(result.isPresent());
+        Assertions.assertEquals(order, result.get());
+    }
+
+    @Test
+    @DisplayName("Метод findById")
+    void findById() {
+        Order order = new Order(1, 1, 1, OrderType.BUY, OrderStatus.OPEN);
+        when(repo.findById(1)).thenReturn(Optional.of(order));
+        Optional<Order> result = orderService.findById(1);
+        Assertions.assertTrue(result.isPresent());
+        Assertions.assertEquals(order, result.get());
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("Метод isOwnOrder")
+    void isOwnOrder() {
+        Person person1 = new Person(3, Role.CLIENT, "Arcady", "1", "Contact1", 0);
+        Person person2 = new Person(4, Role.CLIENT, "notArcady", "1", "Contact2", 0);
+        Order order = new Order(1, person1.getId(), 1, OrderType.SERVICE, OrderStatus.OPEN);
+        when(repo.findById(order.getId())).thenReturn(Optional.of(order));
+        boolean result1 = orderService.isOwnOrder(person1.getId(), order.getId());
+        boolean result2 = orderService.isOwnOrder(person2.getId(), order.getId());
+        Assertions.assertTrue(result1);
+        Assertions.assertFalse(result2);
+    }
+
+    @Test
+    @DisplayName("Метод close")
+    void close() {
+        Order order = new Order(1, 1, 1, OrderType.SERVICE, OrderStatus.OPEN);
+        when(repo.findById(order.getId())).thenReturn(Optional.of(order));
+        orderService.close(1);
+        Assertions.assertEquals(repo.findById(1).get().getStatus(), OrderStatus.CLOSED);
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("Метод delete")
+    void delete() {
+        Order order = new Order(1, 1, 1, OrderType.SERVICE, OrderStatus.OPEN);
+        when(repo.findById(order.getId())).thenReturn(Optional.of(order));
+        orderService.delete(1);
+        Assertions.assertFalse(repo.existsById(1));
+    }
+
+    @Test
+    void findAll() {
+        List<Order> list = List.of(new Order(1, 1, 1, OrderType.SERVICE, OrderStatus.OPEN),
+                new Order(2, 1, 2, OrderType.SERVICE, OrderStatus.OPEN));
+        when(repo.findAll()).thenReturn(list);
+        List<Order> result = orderService.findAll();
+        Assertions.assertEquals(result, list);
+    }
+
+    @Test
+    void findByParameter() {
+        List<Order> list = List.of(new Order(1, 1, 1, OrderType.SERVICE, OrderStatus.OPEN),
+                new Order(2, 1, 2, OrderType.SERVICE, OrderStatus.OPEN));
+        when(repo.findAll()).thenReturn(list);
+        List<Order> result = orderService.findAll();
+        Assertions.assertEquals(result, list);
+    }
+}
